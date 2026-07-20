a3c3479170f1248b093da90ef5dee3900108dc2e
calebszeto <caleb.a.szeto@gmail.com>
2026-07-20 00:01:23 -0700

    Build Etch-A-Sketch Game
    Features a create button to create the grid, the reset button, and
    mouseover eventlisteners to randomly change rgb color of the grid squares.

---
+++ script.js
@@ -0,0 +1,72 @@
+
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn"); 
+const resetBtn = document.querySelector(".reset")
+
+//Make the grid out of squares with percentages to dynamically scale
+function makeEtchSketch(gridLength){
+    const divGridLength = 100/gridLength;
+
+    for(let i = 0; i < gridLength**2; i++){
+        const createDiv = document.createElement("div");
+        createDiv.classList.add("divSquares");
+        createDiv.style.height = `${divGridLength}%`;
+        createDiv.style.width = `${divGridLength}%`;
+
+        container.appendChild(createDiv);
+        createDiv.addEventListener("mouseover",changeColor);
+    }
+
+}
+
+function changeColor(){
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    this.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+
+function popUp(){
+    
+    while(true){
+        let userInput = prompt("Enter the number of squares per side for the new grid.","");
+
+        //Need to account for cancel as parseInt(userInput) will return NaN 
+        if(userInput === null) return;
+
+        userInput = parseInt(userInput);
+
+        if(!Number.isInteger(userInput) || userInput > 100 || userInput <= 0){
+            alert("Enter a integer not greater than 100 and greater than 0.")
+            continue;
+        }
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+        makeEtchSketch(+userInput);
+        break;
+    }
+}
+
+function resetColor(){
+    const divSquares = document.querySelectorAll(".divSquares");
+    divSquares.forEach((element) => {element.style.backgroundColor = "white"});
+}
+
+const gridLength = 16;
+makeEtchSketch(gridLength);
+
+btn.addEventListener("click", popUp)
+resetBtn.addEventListener("click",resetColor)
+
+
+
+
+
+
+
+
+
+
+
+
